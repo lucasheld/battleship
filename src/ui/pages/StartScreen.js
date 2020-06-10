ca2e87e142a251e6d0ccc1ae20538130c12a4a6b
lucasheld <lucasheld@hotmail.de>
2020-06-10 14:42:40 +0200

    used bulma to style StartScreen

--- src/ui/pages/StartScreen.js
+++ src/ui/pages/StartScreen.js
@@ -1,16 +1,14 @@
 import React, { Component } from "react";
-import "./StartScreen.css";
 
 export default class StartScreen extends Component {
     render() {
         return (
             <div>
-                <h1>Seeschlacht</h1>
-                <h2>Version 20.1.7</h2>
+                <h1 className="title">Seeschlacht</h1>
+                <h2 className="subtitle">Version 20.1.7</h2>
                 <h2>Werde Herrscher(in) über die Meere</h2>
                 <br/>
-                <br/>
-                <button onClick={() => this.props.history.push('/playerProfile')}>Neues Spiel</button>
+                <button className="button is-primary" onClick={() => this.props.history.push('/playerProfile')}>Neues Spiel</button>
             </div>
         );
     }
