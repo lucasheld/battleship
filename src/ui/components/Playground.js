9b4da0d695adfc70f116de999ef985992eec32ac
0gain <gianluca.voss@gmx.de>
2020-06-11 01:33:49 +0200

    maps html-playground to datamodel-playground gives each tablecell a unique id (numbers from 0 up)

--- src/ui/components/Playground.js
+++ src/ui/components/Playground.js
@@ -4,23 +4,28 @@ export default class Playground extends Component {
 
     render() {
         let rows = [];
-        let cell = [];
-        cell.push(<td></td>)
+        let cells = [];
+        cells.push(<td key={0} id={0}></td>)
 
-        for (let j = 0; j < 10; j++){
-            let number = 65 + j;
+        for (let j = 1; j < 11; j++){
+            let number = 64 + j;
             let ascii = String.fromCharCode(number)
-            cell.push(<td key={j} id={j}>{ascii}</td>)
+            cells.push(<td key={j} id={j}>{ascii}</td>)
         }
-        rows.push(<tr key={0} id={0}>{cell}</tr>)
+        rows.push(<tr key={0} id={0}>{cells}</tr>)
 
-        for (let i = 0; i < 10; i++){
-            cell = []
-            cell.push(<td>{i + 1}</td>)
-            for (let j = 0; j < 10; j++){
-                cell.push(<td key={j} id={j}></td>)
+        let fields = this.props.player.Playground().fields();
+
+        for (let i = 1; i < 11; i++){
+            cells = []
+            cells.push(<td key={i * 10} id={i * 10}>{i + 1}</td>)
+            for (let j = 1; j < 11; j++){
+                let pos = i * 10 + j;
+                let cell = <td key={pos} id={pos}></td>
+                fields[pos] = cell;
+                cells.push(cell)
             }
-            rows.push(<tr key={i} id={i}>{cell}</tr>)
+            rows.push(<tr key={i} id={i}>{cells}</tr>)
         }
         return (
             <table>
--- src/ui/pages/StrategyModeScreen.js
+++ src/ui/pages/StrategyModeScreen.js
@@ -1,12 +1,13 @@
 import React, { Component } from "react";
 import Playground from "../components/Playground";
+import {player0} from "../../cross-cutting/game";
 
 export default class StrategyModeScreen extends Component {
     render() {
         return (
             <div>
                 StrategyModeScreen
-                <Playground/>
+                <Playground player={player0}/>
             </div>
         );
     }
