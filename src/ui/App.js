8121bc207d170e44d381c3616481f31f6609dbca
lucasheld <lucasheld@hotmail.de>
2020-06-10 14:42:18 +0200

    replaced react-bulma-components with bulma

--- src/ui/App.js
+++ src/ui/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import "bulma/css/bulma.min.css";
 import './App.css';
 import { BrowserRouter, Route } from 'react-router-dom';
 import StartScreen from "./pages/StartScreen";
