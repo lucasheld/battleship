b12cd3a01d4b887bcb4e0513da3f4612418c6a1e
0gain <gianluca.voss@gmx.de>
2020-06-11 12:26:32 +0200

    Puts playground and ships in a table for better structure needs css

--- src/ui/pages/StrategyModeScreen.js
+++ src/ui/pages/StrategyModeScreen.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 import Playground from "../components/Playground";
 import {player0} from "../../cross-cutting/game";
 import Ship from "../components/Ship";
@@ -8,20 +8,69 @@ export default class StrategyModeScreen extends Component {
         return (
             <div>
                 StrategyModeScreen
-                <Playground player={player0}/>
-                <div className="columns is-centered has-text-left">
-                    <Ship id="battleship1" shipLength="5" shipName="Schlachtschiff"/>
-                    <Ship id="cruiser0" shipLength="4" shipName="Kreuzer"/>
-                    <Ship id="cruiser1" shipLength="4" shipName="Kreuzer"/>
-                    <Ship id="destroyer0" shipLength="3" shipName="Zerstörer"/>
-                    <Ship id="destroyer1" shipLength="3" shipName="Zerstörer"/>
-                    <Ship id="destroyer2" shipLength="3" shipName="Zerstörer"/>
-                    <Ship id="submarine0" shipLength="2" shipName="U-Boot"/>
-                    <Ship id="submarine1" shipLength="2" shipName="U-Boot"/>
-                    <Ship id="submarine2" shipLength="2" shipName="U-Boot"/>
-                    <Ship id="submarine3" shipLength="2" shipName="U-Boot"/>
-                </div>
-
+                <table>
+                    <tbody>
+                    <tr>
+                        <td>
+                            <Playground player={player0}/>
+                        </td>
+                        <td>
+                            <table>
+                                <tr>
+                                    <td>
+                                        <Ship id="battleship" shipLength="5" shipName="Schlachtschiff"/>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <Ship id="cruiser0" shipLength="4" shipName="Kreuzer"/>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <Ship id="cruiser1" shipLength="4" shipName="Kreuzer"/>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <Ship id="destroyer0" shipLength="3" shipName="Zerstörer"/>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <Ship id="destroyer1" shipLength="3" shipName="Zerstörer"/>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <Ship id="destroyer2" shipLength="3" shipName="Zerstörer"/>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <Ship id="submarine0" shipLength="2" shipName="U-Boot"/>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <Ship id="submarine1" shipLength="2" shipName="U-Boot"/>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <Ship id="submarine2" shipLength="2" shipName="U-Boot"/>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <Ship id="submarine3" shipLength="2" shipName="U-Boot"/>
+                                    </td>
+                                </tr>
+                            </table>
+                        </td>
+                    </tr>
+                    </tbody>
+                </table>
             </div>
         );
     }
