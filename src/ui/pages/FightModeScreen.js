b4dc3af944abfa589e61fe11aa9db68f8aa964ce
lucasheld <lucasheld@hotmail.de>
2020-06-12 11:52:50 +0200

    implemented players in fight mode screen

--- src/ui/pages/FightModeScreen.js
+++ src/ui/pages/FightModeScreen.js
@@ -7,6 +7,8 @@ import {connect} from "react-redux";
 class FightModeScreen extends Component {
     constructor(props) {
         super(props);
+        this.myPlayer = this.getMyPlayer();
+        this.otherPlayer = this.getOtherPlayer();
         this.unsetFirstRound();
         this.state = {
             redirect: false
@@ -19,11 +21,18 @@ class FightModeScreen extends Component {
         })
     };
 
+    getMyPlayer = () => {
+        return this.props.players.filter(player => player.id === this.props.activePlayerId)[0];
+    };
+
+    getOtherPlayer = () => {
+        return this.props.players.filter(player => player.id !== this.props.activePlayerId)[0];
+    };
+
     unsetFirstRound = () => {
         if(this.props.isFirstRound) {
             this.props.setFirstRound(false);
         }
-        console.log(this.props.isFirstRound)
     };
 
     render() {
@@ -36,7 +45,7 @@ class FightModeScreen extends Component {
                 <h2>Kampfmodus</h2>
                 <br/>
                 <p>
-                    Hallo {null/*.nick TODO*/}
+                    Hallo {this.myPlayer.nick}
                     <br/>
                     Willkommen zurück in der Kommandozentrale
                     <br/>
@@ -44,10 +53,10 @@ class FightModeScreen extends Component {
                 <div className="columns">
                     <div className="column">
                         <p>
-                            Das Meer von {null/*.nick TODO*/}
+                            Das Meer von {this.otherPlayer.nick}
                         </p>
                         <br/>
-                        <Playground player={null/*TODO*/}/>
+                        <Playground player={this.otherPlayer}/>
                         <br/>
                     </div>
                     <div className="column">
@@ -56,7 +65,7 @@ class FightModeScreen extends Component {
                                 Dein Meer
                             </p>
                             <br/>
-                            <Playground player={null/*TODO*/}/>
+                            <Playground player={this.myPlayer}/>
                             <br/>
                         </div>
                     </div>
