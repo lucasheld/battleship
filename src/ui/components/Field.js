407fb6239b3fab7297b882c5cfdaed9b262fd5e7
lucasheld <lucasheld@hotmail.de>
2020-06-13 00:51:01 +0200

    removed unused ship

--- src/ui/components/Field.js
+++ src/ui/components/Field.js
@@ -4,16 +4,12 @@ import {FIELD_TYPES} from "../../redux/actions/field-action";
 import {connect} from "react-redux";
 import {mapStateToProps, matchDispatchToProps} from "../../redux/mapper/field-mapper";
 import FieldClass from "../../redux/data-classes/field";
-import Ship from "./Ship";
 import {fromEvent} from "rxjs";
 
 class Field extends Component {
     constructor(props) {
         super(props);
         this.field = {}
-        this.state = {
-            renderElement: false
-        }
         this.eventMouseMove = null;
     }
 
@@ -26,9 +22,6 @@ class Field extends Component {
     };
 
     fireOnMouseDown = () =>  {
-        this.setState({
-            renderElement: true
-        })
         this.eventMouseMove = fromEvent(document, "mousemove").subscribe(this.handleMouseMove);
         fromEvent(document, "mouseup").subscribe(this.handleMouseUp);
         console.log(this.props.id + " was clicked")
@@ -36,8 +29,10 @@ class Field extends Component {
 
     handleMouseUp = (e) => {
         const element = document.getElementsByClassName("ship-current")[0];
-        this.eventMouseMove.unsubscribe()
-        element.remove();
+        if (element) {
+            this.eventMouseMove.unsubscribe();
+            element.remove();
+        }
     }
 
     handleMouseMove = (e) => {
@@ -57,10 +52,6 @@ class Field extends Component {
     };
 
     render() {
-        if (this.state.renderElement) {
-            return <Ship id="battleship1" shipLength={5} shipName="Schlachtschiff"/>
-        }
-
         this.getOrCreateField();
         return (
             this.props.type === FIELD_TYPES.TEXT ?
