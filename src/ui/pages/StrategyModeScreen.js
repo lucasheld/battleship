a83369e594a897cfebea1331b5e879788b423f0c
Kea Nu <kestue7@gmail.com>
2020-06-13 01:52:23 +0200

    minor change

--- src/ui/pages/StrategyModeScreen.js
+++ src/ui/pages/StrategyModeScreen.js
@@ -43,36 +43,11 @@ class StrategyModeScreen extends Component {
                     </div>
                 </div>
                 <div className="column">
-                    <div className="columns is-centered">
-                        <Ship id="battleship1" shipLength={5} shipName="Schlachtschiff"/>
-                    </div>
-                    <div className="columns is-centered">
-                        <Ship id="cruiser0" shipLength={4} shipName="Kreuzer"/>
-                    </div>
-                    <div className="columns is-centered">
-                        <Ship id="cruiser1" shipLength={4} shipName="Kreuzer" disabled={true}/>
-                    </div>
-                    <div className="columns is-centered">
-                        <Ship id="destroyer0" shipLength={3} shipName="Zerstörer" selected={true}/>
-                    </div>
-                    <div className="columns is-centered">
-                        <Ship id="destroyer1" shipLength={3} shipName="Zerstörer"/>
-                    </div>
-                    <div className="columns is-centered">
-                        <Ship id="destroyer2" shipLength={3} shipName="Zerstörer"/>
-                    </div>
-                    <div className="columns is-centered">
-                        <Ship id="submarine0" shipLength={2} shipName="U-Boot"/>
-                    </div>
-                    <div className="columns is-centered">
-                        <Ship id="submarine1" shipLength={2} shipName="U-Boot"/>
-                    </div>
-                    <div className="columns is-centered">
-                        <Ship id="submarine2" shipLength={2} shipName="U-Boot"/>
-                    </div>
-                    <div className="columns is-centered">
-                        <Ship id="submarine3" shipLength={2} shipName="U-Boot"/>
-                    </div>
+                    {this.props.ships.map( ship =>
+                        <div key={ship.name.toLowerCase() + ship.id} className="columns is-centered">
+                            <Ship id={ship.name.toLowerCase() + ship.id} shipLength={ship.size} shipName={ship.name}/>
+                        </div>
+                    )}
                 </div>
             </div>
         );
