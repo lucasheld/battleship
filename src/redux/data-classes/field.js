2162281ca43daa5e450dec5aee8054b26d5b8925
lucasheld <lucasheld@hotmail.de>
2020-06-14 19:18:23 +0200

    added comments to data classes folder

--- src/redux/data-classes/field.js
+++ src/redux/data-classes/field.js
@@ -1,9 +1,23 @@
+/**
+ * Data class that holds a field.
+ */
 export default class Field {
+    /**
+     * id: The field id
+     * type: The field type
+     * color: The field color
+     * shipIndex: The field index within a ship
+     */
     id;
     type;
     color;
     shipIndex;
 
+    /**
+     * Initializes a new field
+     * @param id: The field id
+     * @param type: The field type
+     */
     constructor(id, type) {
         this.id = id;
         this.type = type;
--- src/redux/data-classes/player.js
+++ src/redux/data-classes/player.js
@@ -1,9 +1,25 @@
+/**
+ * Data class that holds a player.
+ */
 export default class Player {
+    /**
+     * id: The player id
+     * nick: The player name
+     * pin: The player password
+     * avatar: The player avatar seed for an identicon
+     */
     id;
     nick;
     pin;
     avatar;
 
+    /**
+     * Initializes a new player
+     * @param id: The player id
+     * @param nick: The player name
+     * @param pin: The player password
+     * @param avatar: The player avatar seed for an identicon
+     */
     constructor(id, nick, pin, avatar) {
         this.id = id;
         this.nick = nick;
