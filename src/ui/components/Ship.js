60b5e13c94220e86c9f4e6186f7b011950097c02
Kea Nu <kestue7@gmail.com>
2020-06-14 20:19:57 +0200

    added comments to components except for field.js

--- src/ui/components/Ship.js
+++ src/ui/components/Ship.js
@@ -21,7 +21,7 @@ class Ship extends Component {
      */
 
     render() {
-        // define ship background color
+        // Define ships background color
         let backgroundColor;
         if (this.props.ship.disabled) {
             backgroundColor = '#a6a6a6'
@@ -31,7 +31,7 @@ class Ship extends Component {
             backgroundColor = '#8064a2';
         }
 
-        // define ship label color
+        // Defines ships label color
         let labelColor;
         if (this.props.ship.selected) {
             labelColor = '#9bbb59';
@@ -39,6 +39,9 @@ class Ship extends Component {
             labelColor = 'black';
         }
 
+        // Add every cell/div to the ship
+        // Don't paint a color on copies this does the algorithm in field.js
+        // (if it can be placed it uses green if not red)
         let cells = [];
         for (let i = 0; i < this.props.ship.size; i++) {
             let key = this.props.id + "-" + i;
@@ -50,16 +53,21 @@ class Ship extends Component {
             return <Redirect to="/strategy-mode/0"/>;
         }
 
+        // Mark ship as ship-current if it's a copy to find and drag it in field.js
         let className;
         if (this.props.isCopy) {
             className = "ship-current";
         }
 
-        let orientation = this.props.orient[this.props.playground].filter(ship => ship.id === this.props.ship.id && ship.name === this.props.ship.name)[0]
+        // Get orientation of the ship if it's a copy
+        let orientation = this.props.orient[this.props.playground].filter(ship => ship.id === this.props.ship.id && ship.name === this.props.ship.name)[0];
         if (orientation) {
             orientation = orientation.orientation;
         }
 
+        // If ship is a copy don't render the label
+        // Uses the orientation to render it it horizontal or vertical
+        // If it's no copy render it horizontal (real ships to click on on right side)
         return (
             <div className={"columns is-centered " + className} style={{marginBottom: '1px'}}>
                 <div className="column has-text-right">
